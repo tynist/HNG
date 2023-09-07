7db9dcac3ad26064182eccc1a0383efac801081f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-09-07 10:04:58 +0100

    Update main.py

--- stage_one/main.py
+++ stage_one/main.py
@@ -1,36 +1,32 @@
 from flask import Flask, request, jsonify
-from datetime import datetime
-import pytz
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/get_info', methods=['GET'])
 def get_info():
     # Get query parameters
-    slack_name = request.args.get('Peter_Ug')
-    track = request.args.get('backend')
+    slack_name = request.args.get('slack_name', 'Peter_ug')
+    track = request.args.get('track', 'Backend')
 
-    # Get current UTC time with validation of +1 hour
-    current_time = datetime.now(pytz.utc)
-    time_difference = current_time.astimezone(pytz.timezone('UTC+1'))
-    current_day = time_difference.strftime('%A')
+    # Get current day of the week
+    current_day = datetime.datetime.utcnow().strftime('%A')
 
-    # Get GitHub URLs
-    github_url_file = 'https://github.com/tynist/blob/master/yourfile.py'
-    github_url_source = 'https://github.com/yourusername/yourrepository'
+    # Get current UTC time
+    current_time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 
     # Create response JSON
-    response_data = {
-        'slack_name': slack_name,
-        'current_day': current_day,
-        'current_utc_time': str(time_difference),
-        'track': track,
-        'github_url_file': github_url_file,
-        'github_url_source': github_url_source,
-        'status_code': 'Success'
+    response = {
+        "slack_name": slack_name,
+        "current_day_of_week": current_day,
+        "current_utc_time": current_time,
+        "track": track,
+        "github_file_url": "https://github.com/tynist/HNG/blob/main/stage_one/main.py",
+        "github_repo_url": "https://github.com/tynist/HNG/tree/main/stage_one",
+        "status_code": 200
     }
 
-    return jsonify(response_data)
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
